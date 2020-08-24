fc1a0e30af9d381fc42d4f08a7d930725db1540f
TexnoBY <krivosheev2008@gmail.com>
2020-08-24 15:23:28 +0300

    Module7

---
+++ ContosoConf/scripts/pages/schedule.js
@@ -0,0 +1,8 @@
+﻿import { LocalStarStorage } from "../LocalStarStorage.js";
+import { ScheduleList } from "../ScheduleList.js";
+
+
+const element = document.getElementById("schedule");
+const localStarStorage = new LocalStarStorage(localStorage);
+const scheduleLst = new ScheduleList(element, localStarStorage);
+scheduleLst.startDownload();
